260db59409e08c8d9dce37e6a4b7f95e59cac773
Eleilson-Dev <maxta.gamer@gmail.com>
2024-02-25 18:42:15 -0300

    "Tarefa 1 concluída"

---
+++ src/scripts/tarefa1/index.js
@@ -0,0 +1,21 @@
+import { productsList } from './database.js';
+
+const calculateTotalCost = (customerName, products, discount = 0) => {
+  let msg;
+  discount === 0 ? (msg = 'sem desconto') : (msg = `${discount}% de desconto`);
+
+  let total = products
+    .map((element) => {
+      return element.price * element.quantity;
+    })
+    .reduce((acc, current) => {
+      return acc + current;
+    }, 0);
+
+  const discounted = (total * discount) / 100;
+  const totalDisconted = (total - discounted).toFixed(2);
+
+  return `Olá, ${customerName} O total da sua compra é R$ ${totalDisconted} (${msg}).`;
+};
+
+console.log(calculateTotalCost('João', productsList));
