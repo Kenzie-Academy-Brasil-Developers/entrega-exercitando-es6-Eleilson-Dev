936a3afe6189b6c8fd210b8f60f6b480929f53cf
Eleilson-Dev <maxta.gamer@gmail.com>
2024-02-25 19:16:28 -0300

    Tarefa 2 concluída

--- src/scripts/tarefa1/index.js
+++ src/scripts/tarefa1/index.js
@@ -2,20 +2,22 @@ import { productsList } from './database.js';
 
 const calculateTotalCost = (customerName, products, discount = 0) => {
   let msg;
-  discount === 0 ? (msg = 'sem desconto') : (msg = `${discount}% de desconto`);
+  discount === 0
+    ? (msg = '(sem desconto)')
+    : (msg = `(${discount}% de desconto)`);
 
   let total = products
     .map((element) => {
       return element.price * element.quantity;
     })
-    .reduce((acc, current) => {
-      return acc + current;
+    .reduce((acm, current) => {
+      return acm + current;
     }, 0);
 
   const discounted = (total * discount) / 100;
   const totalDisconted = (total - discounted).toFixed(2);
 
-  return `Olá, ${customerName} O total da sua compra é R$ ${totalDisconted} (${msg}).`;
+  return `Olá, ${customerName} O total da sua compra é R$ ${totalDisconted} ${msg}.`;
 };
 
-console.log(calculateTotalCost('João', productsList));
+console.log(calculateTotalCost('João', productsList, 15));
---
+++ src/scripts/tarefa2/index.js
@@ -0,0 +1,55 @@
+import { tasks } from './database.js';
+
+// Função retorna um novo array com apenas as descrições das tarefas
+const getTasksDescriptions = (tasksList) => {
+  const tasksDescriptions = tasksList.map(({ description }) => {
+    return description;
+  });
+
+  return tasksDescriptions;
+};
+
+// console.log(getTasksDescriptions(tasks));
+
+// Função para filtrar tarefas por prioridade
+const filterTasksByPriority = (tasksList, priority) => {
+  const highPriorityTasks = tasksList.filter((taskItem) => {
+    return taskItem.priority === priority;
+  });
+
+  return highPriorityTasks;
+};
+
+// console.log(filterTasksByPriority(tasks, 'alta'));
+
+// Função para obter uma task pelo seu id
+const findTaskById = (tasksList, id) => {
+  const found = tasksList.find((taskItem) => {
+    return taskItem.id === id;
+  });
+
+  return found;
+};
+
+// console.log(findTaskById(tasks, 1));
+
+const removeTask = (tasksList, id) => {
+  const found = tasksList.find((taskItem) => {
+    return taskItem.id === id;
+  });
+
+  if (!found) {
+    console.log('Tarefa não encontrada.');
+  }
+
+  tasksList.forEach((taskItem, index) => {
+    if (taskItem.id === id) {
+      tasksList.splice(index, 1);
+      return;
+    }
+  });
+
+  return tasksList;
+};
+
+console.log(removeTask(tasks, 12));
