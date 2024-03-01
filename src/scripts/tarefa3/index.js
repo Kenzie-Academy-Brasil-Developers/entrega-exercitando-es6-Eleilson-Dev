eb4f062da435637fbfbd8f5a32cdb2763581854d
Mxt@DEV <maxta.gamer@gmail.com>
2024-03-01 12:42:08 -0300

    update

--- src/scripts/tarefa3/index.js
+++ src/scripts/tarefa3/index.js
@@ -13,4 +13,38 @@ const findBooksByCategory = (bookList, category) => {
   });
 };
 
-console.log(findBooksByCategory(bookStoreBooks, 'Aventura'));
+// console.log(findBooksByCategory(bookStoreBooks, 'Aventura'));
+
+const findBookById = (bookList, bookId) => {
+  return bookList.find((book) => {
+    return book.id === bookId;
+  });
+};
+
+// console.log(findBookById(bookStoreBooks, 10));
+
+const sellBook = (bookList, bookId, userType = 0) => {
+  const bookFound = bookList.find((book) => {
+    return book.id === bookId;
+  });
+
+  if (!bookFound || bookFound.quantity === 0) {
+    console.log('Livro indisponível para compra.');
+    return;
+  }
+
+  bookFound.quantity = bookFound.quantity - 1;
+  const discount = bookFound.price * userType;
+  const bookPrice = (bookFound.price - discount).toFixed(2);
+
+  console.log(bookPrice);
+  return `Livro ${bookFound.title} vendido com sucesso por R$ ${bookPrice} (${discount}% de desconto).`;
+};
+
+// console.log(sellBook(bookStoreBooks, 1, userTypeDiscount.platinum));
+
+const calculateAverageRating = () => {
+  return 'x';
+};
+
+console.log(calculateAverageRating());
